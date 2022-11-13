5afb1fda4c058b0616beed5535995daa7bafb4fd
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-11-13 16:46:23 -0600

    Done
    Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 "use strict";
+const buttonList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "*", "=", "/", "c"];
+const inputField = document.getElementById("calc-input");
+
+//skapa eventlyssnare på knapparna med hjälp av id:na
+for (let i = 0; i < buttonList.length; i++) {
+  let targetValue = buttonList[i];
+  //får fram varje elements id
+  let id = "btn-" + buttonList[i];
+
+  //genom id't får vi fram elementet
+  let btnElement = document.getElementById(id);
+
+  //skapar eventlyssnaren
+  if (targetValue === "=") {
+    //då vill vi summera allt i input-fältet
+    btnElement.addEventListener("click", function () {
+      let result = eval(inputField.value);
+      inputField.value = result;
+    });
+  } else if (targetValue === "c") {
+    //då vill vi resna input-fältet
+    btnElement.addEventListener("click", function () {
+      inputField.value = "";
+    });
+  } else {
+    //om knapparna inte är c eller =, händer detta
+    btnElement.addEventListener("click", function () {
+      //lägga till värdet på knappen som vi klickar på i vår input
+      inputField.value += targetValue;
+    });
+  }
+}
